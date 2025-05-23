0e4b7b86ed6d9b76cf2cc2dbf6524c233fb5855f
hj <hj@hjs-MacBook-Pro.local>
2025-05-23 14:39:01 +0900

    UI:댓글컴포넌트생성

--- eatingMark-FE-main/src/pages/homePage.jsx
+++ eatingMark-FE-main/src/pages/homePage.jsx
@@ -4,8 +4,8 @@ import Comment from '../components/Comment';
 function HomePage() {
   return (
     <div>
-      <Comment name="윤혜원" comment="맛있어요" rating={3} />
-      <Comment name="강효정" comment="괜찮네요" rating={3} />
+      <Comment name="윤혜원" comment="맛있어요" />
+      <Comment name="강효정" comment="괜찮네요" />
     </div>
   );
 }
