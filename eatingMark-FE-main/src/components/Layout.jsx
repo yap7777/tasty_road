5008ffde4721627f8386e37adf03987b2da5ca4b
hyewonny2327 <wonny2327@gmail.com>
2025-05-27 15:48:07 +0900

    fix : 중복파일삭제

--- eatingMark-FE-main/src/components/Layout.jsx
+++ eatingMark-FE-main/src/components/Layout.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 function Layout() {
   return (
     <div className="layout">
-      {/* Header란 화면 맨 위에 항상 나오는 고정 메뉴 같은것 예를들면 홈,마이페이지,로그인 같은 메뉴 버튼들 */}
-      {/* <Header /> */}
-      {/* Outlet은 페이지 내용이 바뀌는 공간임 */}
+      <Header></Header>
       <Outlet>Outlet</Outlet>
     </div>
   );
--- eatingMark-FE-main/src/pages/homePage.jsx
+++ eatingMark-FE-main/src/pages/homePage.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Comment from '../components/Comment';
 
 function HomePage() {
-  return (
-    <div>
-      <Comment name="윤혜원" comment="맛있어요" />
-      <Comment name="강효정" comment="괜찮네요" />
-    </div>
-  );
+  return <div>HomePage</div>;
 }
 
 export default HomePage;
