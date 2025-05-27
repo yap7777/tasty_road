7e64a785662078a9faf39533ef1e7b01289dd054
namul21 <k0003706@gmail.com>
2025-05-27 14:53:18 +0900

    folder: folder name변경

--- eatingMark-FE-main/src/components/Header.jsx
+++ eatingMark-FE-main/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Button from './button.jsx';
 
 const Header = () => {
   const [isLogin, setIsLogin] = useState(true);
